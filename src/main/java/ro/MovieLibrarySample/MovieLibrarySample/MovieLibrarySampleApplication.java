c0916821d08239ac027ee7612f995185881a3bd8
Andrei <andrei.pas22@gmail.com>
2019-07-29 16:02:11 +0300

    Initial commit

---
+++ src/main/java/ro/MovieLibrarySample/MovieLibrarySample/MovieLibrarySampleApplication.java
@@ -0,0 +1,13 @@
+package ro.MovieLibrarySample.MovieLibrarySample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieLibrarySampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieLibrarySampleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ro/MovieLibrarySample/MovieLibrarySample/utils/Configuration.java
@@ -0,0 +1,11 @@
+package ro.MovieLibrarySample.MovieLibrarySample.utils;
+
+public class Configuration {
+
+    public static final String DATABASE_URL = "url";
+    public static final int DATABASE_PORT = 3306;
+    public static final String DATABASE_NAME = "mySQLTemplate";
+    public static final String DATABASE_USER = "root";
+    public static final String DATABASE_PASSWORD = "mysqlpassword";
+
+}
